853afe2170565cd5260d079493ef07f8d45f6978
Ryan4012 <246ryan@gmail.com>
2025-09-26 12:58:01 -0700

    Added some keywords, operators, and seperators.

--- lexer.py
+++ lexer.py
@@ -1,1 +1,26 @@
 
+keywords = {'int', 'float', 'double', 'char', 'while', 'if', 'else', 'ADD MORE'}
+operators = {'+', '-', '*', '/', '%', '=', '==', '<', '>', '||', '&&', 'ADD MORE'}
+seperators = {'(', ')', '{', '}', '[', ']', ',', ';', ':', '.', 'ADD MORE'}
+
+
+
+def lex_indentifier(text, i):
+
+
+def lex_integer(text, i):
+
+
+def lex_real(text, i):
+
+
+
+
+def lexer(text, start_index):
+
+
+
+
+
+def main():
+
